a56ea690f8507a2e2e7b135d87f90de4a463e148
Andrei Ionita <andrei.ionita.1902@gmail.com>
2022-06-15 16:53:35 +0300

    Add comments.cpp

---
+++ comments.cpp
@@ -0,0 +1,11 @@
+// This will bring in iostream library
+#include <iostream>
+
+int main(){
+	//One line comment
+	/*
+		Multi-line comment
+		Can't nest block comments tho...
+	*/
+	return 0; // Don't do this
+}
