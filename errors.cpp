91b96bd420c081619993c52b53f269d9207726bc
Andrei Ionita <andrei.ionita.1902@gmail.com>
2022-06-24 12:46:17 +0300

    Add errors file

---
+++ errors.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+	std::cout << "Errors" << std::endl;
+	// Warning: possible runtime error
+	// int value = 7/0;
+	// std::cout << value << std::endl;
+	return 0;
+}
