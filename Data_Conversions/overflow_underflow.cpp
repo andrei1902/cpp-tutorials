7f6fbb426523c685fe2192c9075a112526732e7e
Andrei Ionita <andrei.ionita.1902@gmail.com>
2022-08-30 16:16:19 +0300

    Add overflow_underflow

---
+++ Data_Conversions/overflow_underflow.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main(){
+	unsigned char data {250};
+
+	++data;
+	std::cout << "data : " << static_cast<int>(data) << std::endl;
+
+	++data;
+	std::cout << "data : " << static_cast<int>(data) << std::endl;
+
+	++data;
+	std::cout << "data : " << static_cast<int>(data) << std::endl;
+
+	++data;
+	std::cout << "data : " << static_cast<int>(data) << std::endl;
+
+	++data; 
+	std::cout << "data : " << std::hex <<  static_cast<int>(data) << std::endl; // 255
+
+	++data;  // Overflow
+	std::cout << "data : " << static_cast<int>(data) << std::endl; // 256
+
+	std::cout << std::dec ;
+
+	data = 1;
+
+	--data; // Underflow
+	std::cout << "data : " << static_cast<int>(data) << std::endl;
+
+	--data;
+	std::cout << "data : " << static_cast<int>(data) << std::endl;
+
+	return 0;
+}
