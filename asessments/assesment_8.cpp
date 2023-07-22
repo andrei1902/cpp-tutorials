15c647cc23fd0bb3213a679f7f2d7d24b6f12d1e
Andrei Ionita <andrei.ionita.1902@gmail.com>
2023-07-22 16:20:32 +0300

    Add assesment_8

---
+++ asessments/assesment_8.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using std::boolalpha;
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+	int minX = -20;
+	int maxX = 20;
+	int minY = -10;
+	int maxY = 10;
+	int xLocation;
+	int yLocation;
+	cout << "Welcome to territory control. Please type in your x and y position" << endl;
+	cout << "Type in your x location:" << endl;
+	cin >> xLocation;
+	cout << endl;
+	cout << "Type in your y location:" << endl;
+	cin >> yLocation;
+	cout << endl;
+
+	if (xLocation > minX && xLocation < maxX && yLocation > minY && yLocation < maxY) {
+		cout << "You are completely sorrounded. Don't move." << endl;
+		return 0;
+	}
+	cout << "You are out of reach!" << endl;
+	return 0;
+}
